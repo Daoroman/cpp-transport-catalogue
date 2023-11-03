190d9db4329a5cdeb2ade8aefa764f3caeb69e47
Daoroman <132341235+Daoroman@users.noreply.github.com>
2023-11-03 09:28:46 +0700

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,27 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+
+#include "geo.h"
+#include "transport_catalogue.h"
+
+namespace transport_catalog {
+	
+	namespace input_detail {
+
+		struct InputDataBus {
+			std::string name;
+			std::vector<std::string> stops;
+			bool circle;
+		};
+
+	}
+	
+	using namespace detail;
+
+	void ReadData(std::istream& in, TransportCatalogue& catalog);
+	void FillDistance(std::pair<std::string, std::string>& stop, TransportCatalogue& catalog);
+}
