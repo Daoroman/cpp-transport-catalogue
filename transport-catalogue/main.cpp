9d891d05cf90de7e7bfdbe92183a9c68ee417f1a
Daoroman <132341235+Daoroman@users.noreply.github.com>
2023-11-03 09:29:04 +0700

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,11 @@
-// место для вашего кода
+#include "stat_reader.h"
+#include "input_reader.h"
+
+using namespace transport_catalog;
+
+int main() {
+    TransportCatalogue catalogue;
+    std::ostream& output = std::cout;
+    ReadData(std::cin, catalogue);
+    ProcessRequests(output, catalogue);
+}
