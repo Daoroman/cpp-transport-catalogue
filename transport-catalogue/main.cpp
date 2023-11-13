b78eed907215485f506d8a1078c671df3c3f46af
Daoroman <132341235+Daoroman@users.noreply.github.com>
2023-11-13 21:55:35 +0700

    Add files via upload

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,20 +1,18 @@
 #include "json_reader.h"
 #include "request_handler.h"
 
-
 int main() {
+    transport_catalog::TransportCatalogue catalogue;
+    JsonReader json_doc(std::cin);
+    
+    
+    json_doc.FillRequests();
+    json_doc.FillCatalogue(catalogue);
 
-transport_catalog ::TransportCatalogue catalogue;
-   
-JsonReader json_doc(std::cin);
-json_doc.FillCatalogue(catalogue);
+    const auto& stat_requests = json_doc.GetStatRequests();
+    const auto& render_settings = json_doc.GetRenderSettings().AsMap();
+    const auto& renderer = json_doc.FillRenderSettings(render_settings);
 
-const auto& stat_requests = json_doc.GetStatRequests();
-const auto& render_settings = json_doc.GetRenderSettings().AsMap();
-const auto& renderer = json_doc.FillRenderSettings(render_settings);
-    
-RequestHandler rh(catalogue, renderer);
-    
-rh.RequestOut(stat_requests);
-    
+    RequestHandler  requesthandler(catalogue, renderer);
+    json_doc.RequestOut(stat_requests, requesthandler);
 }
